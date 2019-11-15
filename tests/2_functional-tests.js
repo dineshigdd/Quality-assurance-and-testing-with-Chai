73a6a15dc55e9466bfa88fdae6a8cc5d49eabe5d
Glitch (phase-pigment) <none>
2019-11-15 10:33:23 +0000

    🍙🌻 Checkpoint ./tests/2_functional-tests.js:6079345/266

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -145,11 +145,11 @@ suite('Functional Tests', function() {
             assert.equal(res.status,200,'response status should be 200');
             assert.equal(res.type, 'application/json', "Response should be json");
             assert.equal(res.body.name,'Giovanni');
-            assert.equal(res.body.surname,'da Verrazzano');
+            assert.equal(res.body.surname,'da Verrazzano');    
           done();
-          })       
-        
+          })             
       });
+      
     });
 
   });
